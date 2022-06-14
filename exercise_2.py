2f3eb2261279352ebec7695c94f9b84db5979bdb
Radar3759 <bunnybridgemitten@gmail.com>
2022-06-14 14:40:04 -0700

    -update README add ex2

---
+++ exercise_2.py
@@ -0,0 +1,15 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.")  # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+#print("This won't run.")
+
+print("This will run.")
+
+# STUDY DRILLS
+# 1. Check if you were right about what # does
+# 2. Proofread your text backwards
+# 3 If you found mistakes, fix them
+# 4. Read what you typed aloud including character names. Fix any mistakes you find
