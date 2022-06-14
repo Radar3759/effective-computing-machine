c03b242714b0208ed70514d1b48b7c284a391625
Radar3759 <bunnybridgemitten@gmail.com>
2022-06-14 15:51:17 -0700

    -add ex 3, update readme

---
+++ exercise_3.py
@@ -0,0 +1,68 @@
+#makes the docstring error go away per Stack Overflow
+'''useful docstring'''
+#prints the words in quotes
+print("I will now count my chickens:")
+#prints the words in quotes and does maths
+print("Hens", 25 + 30 / 6)
+#prints the words in quotes and does maths
+print("Roosters", 100 - 25 * 3 % 4)
+#prints the words n quotes
+print("Now I will count the eggs:")
+#does maths, prints answer
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#prints the words in quotes
+print("Is it true that 3 + 2 < 5 - 7?")
+#does maths, prints answer
+print(3 + 2 < 5 - 7)
+#prints the words in quote, does maths, print answer
+print("What is 3 + 2?", 3 + 2)
+#prints the words in quote, does maths, print answer
+print("What is 5 - 7?", 5 - 7)
+#prints the words in quotes
+print("Oh, that's why it's False.")
+#prints the words in quotes
+print("How about some more.")
+#prints the words in quote, does maths, print answer
+print("Is it greater?",-2 < 5)
+#prints the words in quote, does maths, print answer
+print("Is it greater or equal?", -2 <= 5)
+
+#STUDY DRILLS
+#1. comment each line
+#2. Redo exercise 0? (It was install Python)
+#3 Write a py file to calculate something (see below)
+#4. Rewrite Exercise 3 with float point numbers  (see below)
+
+#CALCULATE SOMETHING (constants not used on purpose)
+bill_amount = 25.00
+tip_amount = bill_amount * .15
+total_amount = tip_amount + bill_amount
+
+print("Study Question #3, Calculate something." )
+print(f"\nThe total bill is {total_amount}.\n")
+#FLOAT POINT NUMS
+print("I will now count my chickens:")
+#prints the words in quotes and does maths
+print("Hens", float(25 + 30 / 6))
+#prints the words in quotes and does maths
+print("Roosters", float( 100 - 25 * 3 % 4))
+#prints the words n quotes
+print("Now I will count the eggs:")
+#does maths, prints answer
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+#prints the words in quotes
+print("Is it true that 3 + 2 < 5 - 7?")
+#does maths, prints answer
+print(float(3 + 2 < 5 - 7))
+#prints the words in quote, does maths, print answer
+print("What is 3 + 2?", float(3 + 2))
+#prints the words in quote, does maths, print answer
+print("What is 5 - 7?", float(5 - 7))
+#prints the words in quotes
+print("Oh, that's why it's False.")
+#prints the words in quotes
+print("How about some more.")
+#prints the words in quote, does maths, print answer
+print("Is it greater?",-2 < 5)
+#prints the words in quote, does maths, print answer
+print("Is it greater or equal?", -2 <= 5)
