4194c46f623e155a1dbf3741dea3010f44725a01
Radar3759 <bunnybridgemitten@gmail.com>
2022-06-14 13:47:04 -0700

    -update readme add ex 1

---
+++ Exercise_1.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+#STUDY DRILLS BELOW
+#make your script print another line
+print("another line")
+
+#to make the script only print one of these lines, comment out the other lines
+
+#use the octothorpe/pound/hash/mesh/etc symbol "#" to comment out a line in Py
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # effective-computing-machine
 Learn Python 3 The Hard Way Exercises
+- Ex 1 and Study Drills
+- 
