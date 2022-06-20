c91ca3263767dcbcaead623714750f889f678e6b
Radar3759 <bunnybridgemitten@gmail.com>
2022-06-20 15:24:35 -0700

    -add ex10

---
+++ exercise_10.py
@@ -0,0 +1,17 @@
+'''useful docstring'''
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+\n\t\t- Catnip
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
